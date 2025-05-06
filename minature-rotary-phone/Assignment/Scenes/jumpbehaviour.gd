36e3c19b9b84927804a7f5913a88ba7ebf8d802f
leeCox <14360547+leecox@user.noreply.gitee.com>
2025-05-06 19:11:20 +0100

    scene manager

--- minature-rotary-phone/Assignment/Scenes/jumpbehaviour.gd
+++ minature-rotary-phone/Assignment/Scenes/jumpbehaviour.gd
@@ -1,19 +1,16 @@
 class_name Jump extends SteeringBehavior
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 @export var jump_strength := 10
-@export var jump_interval := 1.0  # seconds between jumps
+@export var jump_interval := 2.0  # seconds between jumps
 
 var time_since_last_jump := 0.0
 
-func calculate() -> Vector3:
-	if !enabled:
-		return Vector3.ZERO
-
-
+func _physics_process(delta: float) -> void:
 	time_since_last_jump += get_physics_process_delta_time()
-
+	get_parent().velocity.y += 600
 	if time_since_last_jump >= jump_interval:
-		time_since_last_jump = 0.0
-		return Vector3.UP * jump_strength
-	else:
-		return Vector3.ZERO
+		animation_player.play("Jump")
+		animation_player.animation_set_next("Jump", "Walking")
+		jump_interval = randf_range(1.0, 3.0)
+		time_since_last_jump = 0
